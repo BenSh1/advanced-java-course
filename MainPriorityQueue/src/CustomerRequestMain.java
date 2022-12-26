ed52a9046aa3f365c01cb845b08246212698b093
BenSh1 <47560422+BenSh1@users.noreply.github.com>
2022-12-26 09:37:47 +0200

    Update CustomerRequestMain.java

--- MainPriorityQueue/src/CustomerRequestMain.java
+++ MainPriorityQueue/src/CustomerRequestMain.java
@@ -1,43 +1,53 @@
+import java.util.Random;
 
 public class CustomerRequestMain {
 	public static void main(String[] args)
 	{
-		CustomerRequest ben = new CustomerRequest("Ben" , 3150 , "I want a phone call.");
-		CustomerRequest may = new CustomerRequest("May" , 3156 , "When are you going to add [feature]?");
-		CustomerRequest gil = new CustomerRequest("gil" , 3158 , "I want a refund!");
-		CustomerRequest matan = new CustomerRequest("matan" , 3159 , "Tweet me back.");
-		CustomerRequest johny = new CustomerRequest("johny" , 3158 , "When do you close?");
-		CustomerRequest dummy = new CustomerRequest("dummy" , 3150 , "I want a phone call, now!");
-		CustomerRequest dummy2 = new CustomerRequest("dummy2" , 3150 , "Check");
 
-		GenericPriorityQueue<CustomerRequest> CustomerpriorQueue = new GenericPriorityQueue<CustomerRequest>(5);
-		System.out.println("-----------------add-------------------");
+		final int NUMBER_OF_REQUEST = 5;
+		Random rand = new Random();
+
+		String[] names = {"Ben" , "Chen","Matan" , "May" , "Johny"};
+		String[] detailsOfRequest = {"I want a phone call.", "When are you going to add [feature]?" 
+								, "I want a refund!","When do you close?","When do you open?"};
+
+		CustomerRequest requests[] = new CustomerRequest[NUMBER_OF_REQUEST];
 		
-		CustomerpriorQueue.add(ben, 2);
-		CustomerpriorQueue.add(may, 2);
-		CustomerpriorQueue.add(gil, 4);
-		CustomerpriorQueue.add(matan, 1);
-		CustomerpriorQueue.add(johny, 1);
-
-		//CustomerpriorQueue.add(dummy, 5);
-
+		for (int i = 0; i < NUMBER_OF_REQUEST; i++) {
+			//int randomLastDigitId = rand.nextInt(10);
+			requests[i] = new CustomerRequest(names[i], 800800800 + i/*randomLastDigitId*/, detailsOfRequest[i]);
+		}
+		
+		int maxOfPriority = rand.nextInt(10) + 1;
+		GenericPriorityQueue<CustomerRequest> CustomerpriorQueue = new GenericPriorityQueue<CustomerRequest>(maxOfPriority);
+		
+		System.out.println("-----------------add-------------------");
+		int levelOfPriority;
+		for (int i = 0; i < requests.length ; i++) {
+			levelOfPriority = rand.nextInt(maxOfPriority);
+			CustomerpriorQueue.add(requests[i] ,levelOfPriority );
+		}
+		
 		System.out.println(CustomerpriorQueue.toString());
 		
 		System.out.println("-----------------poll-------------------");
 		CustomerRequest headCustomer = CustomerpriorQueue.poll();
-		System.out.println("head : " + headCustomer.toString());
+		System.out.println("The Customer head : \""+ headCustomer.toString()+"\" is deleted");
+
 		System.out.println(CustomerpriorQueue.toString());
 		
 		System.out.println("-----------------contains-------------------");
-		System.out.println(CustomerpriorQueue.contains(ben));
-		System.out.println(CustomerpriorQueue.contains(johny));
-		System.out.println(CustomerpriorQueue.contains(dummy));
-		System.out.println(CustomerpriorQueue.contains(may));
-		System.out.println(CustomerpriorQueue.contains(dummy2));
+		CustomerRequest Kayle = new CustomerRequest("Kayle" , 123456789 , "Checking if it's working");
+		CustomerRequest Lara = new CustomerRequest("Lara" , 800800801 , "When are you going to add [feature]?");
+
+		System.out.println("Is the customer \"Kayle\" in the Queue? " + CustomerpriorQueue.contains(Kayle));
+		System.out.println("Is the customer \""+ requests[0].getName() +"\" in the Queue? " + CustomerpriorQueue.contains(requests[0]));
+		System.out.println("Is the customer \"Lara\" in the Queue? " + CustomerpriorQueue.contains(Lara));
 
 		System.out.println("-----------------remove-------------------");
-		System.out.println(CustomerpriorQueue.remove(gil));
-		System.out.println(CustomerpriorQueue.remove(dummy));
+		System.out.println("Do the customer \"Kayle\" is removed? " +CustomerpriorQueue.remove(Kayle));
+		System.out.println("Do the customer \""+ requests[0].getName() +"\" is removed? " + CustomerpriorQueue.remove(requests[0]));
+		System.out.println("Do the customer \"Lara\" is removed? " +CustomerpriorQueue.remove(Lara));
 
 		System.out.println(CustomerpriorQueue.toString());
 		
